63e55d717acb406d50301f5c1fa19f593c11f540
labike <18091686891@sina.cn>
2018-03-09 22:24:09 +0800

    new initial

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: labike 
+ * @Date: 2018-03-09 14:12:55 
+ * @Last Modified by: labike
+ * @Last Modified time: 2018-03-09 22:12:43
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+    <div>
+        <h1>Hello, world!</h1>
+    </div>,
+    document.getElementById('app')
+);
